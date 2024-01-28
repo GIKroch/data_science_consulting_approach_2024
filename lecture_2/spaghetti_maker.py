80f43607a933adc8520ab6142ff91c70d3e47173
gkroch01 <gkroch01@kearney.com>
2024-01-28 18:16:34 +0100

    Basic version for lecture 2

---
+++ lecture_2/spaghetti_maker.py
@@ -0,0 +1,18 @@
+import json
+
+def read_ingredients(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+def make_spaghetti(recipe_name, ingredients):
+    if recipe_name in ingredients:
+        print(f"Making {recipe_name} Spaghetti with the following ingredients:")
+        for step, ingredient in ingredients[recipe_name].items():
+            print(f"{step}: {ingredient}")
+    else:
+        print("Recipe not found.")
+
+if __name__ == "__main__":
+    ingredients = read_ingredients("ingredients.json")
+    # Example: Making Carbonara
+    make_spaghetti("Carbonara", ingredients)

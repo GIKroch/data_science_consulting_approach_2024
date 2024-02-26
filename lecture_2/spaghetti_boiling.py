1d53f98918e3c61ef50002276cc024967ea734dd
gkroch01 <gkroch01@kearney.com>
2024-02-26 19:26:01 +0100

    adjustment

---
+++ lecture_2/spaghetti_boiling.py
@@ -0,0 +1,10 @@
+def boil_water(time, spaghetti_type):
+    """Boiling water function
+
+    Parameters
+    ----------
+    time : _type_
+        _description_
+    """
+
+    print(f"Boiling {spaghetti_type} takes {time}")

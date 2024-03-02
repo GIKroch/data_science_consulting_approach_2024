1c2cb3a757a49679315887454740a1e64b98c710
gkroch01 <gkroch01@kearney.com>
2024-03-02 17:56:34 +0100

    All generators created

--- lecture_3/data_generator/main.py
+++ lecture_3/data_generator/main.py
@@ -1,7 +1,10 @@
 from bom_generator import BomGenerator
 from procurement_generator import ProcurementGenerator
+from client_generator import generate_client_prices
+from sales_generator import generate_sales
 
 if __name__=='__main__':
     bom = BomGenerator().generate()
     procurement = ProcurementGenerator().generate()
-
+    client_prices = generate_client_prices(bom, procurement)
+    sales_data = generate_sales(bom)

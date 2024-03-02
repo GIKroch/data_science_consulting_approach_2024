c692f03206421cd94f2206c2f3d8c60fda5b0e01
gkroch01 <gkroch01@kearney.com>
2024-03-02 18:04:35 +0100

    Adding saver function

--- lecture_3/data_generator/helpers.py
+++ lecture_3/data_generator/helpers.py
@@ -5,3 +5,6 @@ def random_date(year):
     start = datetime(year, 1, 1)
     end = datetime(year, 12, 31)
     return start + (end - start) * random.random()
+
+def data_saver(df, filename):
+    df.to_excel(filename, index = False)
--- lecture_3/data_generator/main.py
+++ lecture_3/data_generator/main.py
@@ -2,9 +2,15 @@ from bom_generator import BomGenerator
 from procurement_generator import ProcurementGenerator
 from client_generator import generate_client_prices
 from sales_generator import generate_sales
+from helpers import data_saver
 
 if __name__=='__main__':
     bom = BomGenerator().generate()
     procurement = ProcurementGenerator().generate()
     client_prices = generate_client_prices(bom, procurement)
     sales_data = generate_sales(bom)
+
+    data_saver(bom, "boms.xlsx")
+    data_saver(procurement, "procurement.xlsx")
+    data_saver(client_prices, "client_prices.xlsx")
+    data_saver(sales_data, "sales.xlsx")
